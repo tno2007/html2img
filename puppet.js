dc203ac27230bb9b3b5e0f4f4b2a9e33e7896084
tno2007 <vanderlindel@gmail.com>
2018-07-13 08:37:14 +0200

    fgfgfkhjgh

--- puppet.js
+++ puppet.js
@@ -1,48 +1,54 @@
-'use strict';
+"use strict";
 
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
-(async() => {
-
-    const browser = await puppeteer.launch({
-        headless: true,
-        ignoreHTTPSErrors: true,
-        timeout: 1000
-    });
-    const page = await browser.newPage();
+(async () => {
 	
-	await page.setViewport({
-		width: 1920, 
-		height: 1080
-	});
+	try {
+		  const browser = await puppeteer.launch({
+			headless: true,
+			ignoreHTTPSErrors: true,
+			timeout: 1000,
+			args:['--no-sandbox']
+		  });
+		  const page = await browser.newPage();
 
-
-
-	/*
-    await page.setRequestInterceptionEnabled(true);
-    page.on('request', request => {
-        if (request.resourceType === 'Script') {
-            request.abort();
-        } else {
-            request.continue();
-        }
-    });
-	*/
-
-    await page.goto('https://iriesoft.co.za/public/grid.html');
-    /*
-	await page.screenshot({
-		path: 'screenshot.png',
-        clip: {
-			x: 0, 
-			y:0, 
-			width: 1980, 
+		  await page.setViewport({
+			width: 1920,
 			height: 1080
-		}
-    });
-	*/
-    await page.screenshot({path: 'screenshot-full.png', fullPage: true});
+		  });
 
-    browser.close();
+		  /*
+			await page.setRequestInterceptionEnabled(true);
+			page.on('request', request => {
+				if (request.resourceType === 'Script') {
+					request.abort();
+				} else {
+					request.continue();
+				}
+			});
+			*/
 
-})();
+		  await page.goto("https://iriesoft.co.za/public/grid.html");
+		  /*
+			await page.screenshot({
+				path: 'screenshot.png',
+				clip: {
+					x: 0, 
+					y:0, 
+					width: 1980, 
+					height: 1080
+				}
+			});
+			*/
+		  await page.screenshot({ path: "screenshot.png", fullPage: true });
+
+		  browser.close();		
+	}
+	catch(err) {
+		console.log(err);
+	}
+	
+
+
+  })();
